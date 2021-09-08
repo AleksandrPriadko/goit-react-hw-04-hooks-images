05f8650fbe190471bc2b601f191ba5a26a73e228
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2021-09-08 23:45:51 +0300

    ref app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Searchbar from "./components/Searchbar";
 import ApiGallery from "./components/APIGallery";
 import ImageGallery from "./components/ImageGallery";
 
-class App extends Component {
-  state = {
-    gallery: [],
-    searchQuery: "",
-    page: 1,
-    isLoader: false,
-    error: null,
+export default function App(params) {
+  const [gallery, setGallery] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [isLoader, setIsLoader] = useState(false);
+  const [error, setError] = useState(null);
+  console.log(searchQuery);
+
+  useEffect(() => {
+    fetchGallery();
+  }, [searchQuery]);
+
+  const FormSubmitHandler = ({ name }) => {
+    //console.log(name);
+    setSearchQuery(name);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchGallery();
-    }
-  }
-
-  FormSubmitHandler = (query) => {
-    this.setState({
-      searchQuery: query,
-      page: 1,
-      gallery: [],
-      error: null,
-    });
-  };
-
-  fetchGallery = (event) => {
-    const { searchQuery, page } = this.state;
-    this.setState({ isLoader: true });
-
-    ApiGallery(searchQuery.name, page)
+  const fetchGallery = (event) => {
+    setIsLoader(!isLoader);
+    ApiGallery(searchQuery, page)
       .then((hits) => {
-        this.setState((prevState) => ({
-          gallery: [...prevState.gallery, ...hits],
-          page: prevState.page + 1,
-        }));
+        console.log(hits);
+        setGallery((prev) => [...prev, ...hits]);
+        setPage((prev) => prev + 1);
       })
       .then(() => {
         if (page > 1)
@@ -45,24 +35,82 @@ class App extends Component {
             behavior: "smooth",
           });
       })
-      .catch((error) => this.setState({ error }))
-      .finally(() => this.setState({ isLoader: false }));
+      .catch((error) => setError(error))
+      .finally(() => setIsLoader((prev) => !prev));
   };
 
-  render() {
-    const { gallery, isLoader } = this.state;
-
-    return (
-      <>
-        <Searchbar onSubmit={this.FormSubmitHandler} />
-        <ImageGallery
-          gallery={gallery}
-          onFetchGallery={this.fetchGallery}
-          isLoader={isLoader}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSubmit={FormSubmitHandler} />
+      <ImageGallery
+        gallery={gallery}
+        onFetchGallery={fetchGallery}
+        isLoader={isLoader}
+      />
+    </>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     gallery: [],
+//     searchQuery: "",
+//     page: 1,
+//     isLoader: false,
+//     error: null,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       this.fetchGallery();
+//     }
+//   }
+
+//   FormSubmitHandler = (query) => {
+//     this.setState({
+//       searchQuery: query,
+//       page: 1,
+//       gallery: [],
+//       error: null,
+//     });
+//   };
+
+//   fetchGallery = (event) => {
+//     const { searchQuery, page } = this.state;
+//     this.setState({ isLoader: true });
+
+//     ApiGallery(searchQuery.name, page)
+//       .then((hits) => {
+//         this.setState((prevState) => ({
+//           gallery: [...prevState.gallery, ...hits],
+//           page: prevState.page + 1,
+//         }));
+//       })
+//       .then(() => {
+//         if (page > 1)
+//           window.scrollTo({
+//             top: document.documentElement.scrollHeight,
+//             behavior: "smooth",
+//           });
+//       })
+//       .catch((error) => this.setState({ error }))
+//       .finally(() => this.setState({ isLoader: false }));
+//   };
+
+//   render() {
+//     const { gallery, isLoader } = this.state;
+
+//     return (
+//       <>
+//         <Searchbar onSubmit={this.FormSubmitHandler} />
+//         <ImageGallery
+//           gallery={gallery}
+//           onFetchGallery={this.fetchGallery}
+//           isLoader={isLoader}
+//         />
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
