516aaa68f580ef578194880c5f52380e3ad0a6ba
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2021-09-09 21:54:59 +0300

    deploy gh pages

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
 
   useEffect(() => {
     fetchGallery();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchQuery]);
 
   const FormSubmitHandler = (query) => {
